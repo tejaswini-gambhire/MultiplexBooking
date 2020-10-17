46e1b77d9b9720f9d6a6adaedc4ecc0fd3394a55
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2020-10-17 14:37:49 +0530

    Added devise gem and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_064900) do
+ActiveRecord::Schema.define(version: 2020_10_17_090611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,20 @@ ActiveRecord::Schema.define(version: 2020_10_17_064900) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["movie_id"], name: "index_shows_on_movie_id"
     t.index ["screen_id"], name: "index_shows_on_screen_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "mobile_number"
+    t.string "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "seats", "screens"
